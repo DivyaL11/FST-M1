78df47d4397483e8d773d619bdb1c6e8d4fe51da
DivyaL11 <divyal11@in.ibm.com>
2024-06-18 13:17:29 +0530

    Add files via upload

--- Appium/Activities/Activity1.java
+++ Appium/Activities/Activity1.java
@@ -7,55 +7,52 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Activity1 {
-    //Driver Declaration
-    AndroidDriver driver;
+    public class Activity1 {
+        // Driver Declaration
+        AndroidDriver driver;
 
-    //Set up method
+        // Set up method
+        @BeforeClass
+        public void setUp() throws MalformedURLException {
+            // Desired Capabilities
+            UiAutomator2Options options = new UiAutomator2Options();
+            options.setPlatformName("android");
+            options.setAutomationName("UiAutomator2");
+            options.setAppPackage("com.android.calculator2");
+            options.setAppActivity(".Calculator");
+            options.noReset();
 
-    @BeforeClass
-    public void setUp() throws MalformedURLException {
+            // Server Address
+            URL serverURL = new URL("http://localhost:4723/");
 
-        //Set Capabilities
-        UiAutomator2Options caps = new UiAutomator2Options()
-                .setPlatformName("Android")
-                .setAutomationName("UiAutomator2")
-                .setAppPackage("com.sec.android.app.popupcalculator")
-                .setAppActivity(".Calculator")
-                .noReset();
+            // Driver Initialization
+            driver = new AndroidDriver(serverURL, options);
+        }
 
-        //Set the server URL
-        URL serverURL = new URL("http://localhost:4723/wb/hub");
+        // Test method
+        @Test
+        public void multiplyTest() {
+            // Perform the calculation
+            driver.findElement(AppiumBy.id("digit_5")).click();
+            driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+            driver.findElement(AppiumBy.id("digit_8")).click();
+            driver.findElement(AppiumBy.accessibilityId("equals")).click();
 
-        //Initialize the Android driver
-        driver = new AndroidDriver(serverURL, caps);
+            // Find the result
+            String result = driver.findElement(AppiumBy.id("result")).getText();
 
+            // Assertion
+            Assert.assertEquals(result, "40");
+        }
+
+
+        // Tear down method
+        @AfterClass
+        public void tearDown() {
+            // Close the app
+            driver.quit();
+        }
     }
-
-    @Test
-    public void multiplyTest() {
-        //Find the 7 and tap it
-        driver.findElement(AppiumBy.id("com.sec.android.app.popupcalculator:id/bt_07")).click();
-        //Find the multiply symbol and tap it
-        driver.findElement(AppiumBy.accessibilityId("Multiplication")).click();
-        //Find the 6 and tap it
-        driver.findElement(AppiumBy.id("com.sec.android.app.popupcalculator:id/bt_06")).click();
-        //Find the equalsto symbol and tap it
-        driver.findElement(AppiumBy.accessibilityId("Equal")).click();
-
-        //Assertions
-        String result = driver.findElement(AppiumBy.id("txtCalc")).getText();
-        Assert.assertEquals(result, "42");
-    }
-
-    @AfterClass
-    public void tearDown() {
-        //close the app
-        driver.quit();
-    }
-}
-
